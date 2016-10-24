7a210ac8e337f541c7ef2e03748b966cc4cc5e42
robertholf <rob@bertholf.com>
2016-10-24 13:12:27 -0700

    Initial Language Files

---
+++ resources/lang/en/http.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| HTTP Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used in the views/errors files.
+	|
+	*/
+
+	'404' => [
+		'title' => 'Page Not Found',
+		'description' => 'Sorry, but the page you were trying to view does not exist.',
+	],
+
+	'500' => [
+		'title' => 'Ruh Roh!',
+		'description' => 'Looks like something went wrong... but we will fix it!',
+	],
+
+	'503' => [
+		'title' => 'Be right back.',
+		'description' => 'Be right back.',
+	],
+
+	'701' => [
+		'title' => 'Data Not Found.',
+		'description' => 'Data Not Found.',
+	],
+
+];
